2e2c92e65e603219efabe14b6915c7d4c3924965
John Osterman <josterman@pewtrusts.org>
2019-01-17 15:44:18 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+import Router from '../router/';
+export default class PCTApp {
+    init(subscriptionsForRouter, PubSubSetter, setHashFn, decodeHashFn, views){
+        
+        this.router = new Router();
+        this.router.init(subscriptionsForRouter, PubSubSetter, setHashFn, decodeHashFn, views);
+        this.disableHoverOnTouch();
+    }
+    disableHoverOnTouch(){
+    // HT: https://stackoverflow.com/a/30303898
+        var hasHoverClass = false;
+        var container = document.body;
+        var lastTouchTime = 0;
+
+        function enableHover() {
+            // filter emulated events coming from touch events
+            if (new Date() - lastTouchTime < 500) return;
+            if (hasHoverClass) return;
+
+            container.classList.add('has-hover');
+            hasHoverClass = true;
+        }
+
+        function disableHover() {
+            if (!hasHoverClass) return;
+            container.classList.remove('has-hover');
+            hasHoverClass = false;
+        }
+
+        function updateLastTouchTime() {
+            lastTouchTime = new Date();
+        }
+
+        document.addEventListener('touchstart', updateLastTouchTime, true);
+        document.addEventListener('touchstart', disableHover, true);
+        document.addEventListener('mousemove', enableHover, true);
+
+        enableHover();
+    }
+}
